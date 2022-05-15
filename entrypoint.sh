c0095b7d9c5f04a1df4fae70dea85dcea68b005e
Rebekah Rowe <rebekahrowe9999@gmail.com>
2022-05-15 15:43:12 -0400

    No longer repack iso & added iso format info to fix crashes apon loading level

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ mkdir -vp "$XDG_DATA_HOME/dolphin-emu/Load/Textures/" "$XDG_CONFIG_HOME/dolphin-
 cp -vn /app/dolphin-configs/Config/* "$XDG_CONFIG_HOME/dolphin-emu/"
 cp -vn /app/dolphin-configs/GameSettings/* "$XDG_DATA_HOME/dolphin-emu/"
 ln -vs /app/dolphin-configs/Load/Textures/GM5E7D "$XDG_DATA_HOME/dolphin-emu/Load/Textures/" || true
-dolphin-emu -e /app/metal-arms.iso
+dolphin-emu -e /app/metal-arms/sys/main.dol
